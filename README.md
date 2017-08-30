2d7f272438a84607c82722a8f558744ff418bcab
Stanley Yang <me@stanleycyang.com>
2017-08-29 21:19:25 -0700

    Added cronjob and README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sendy-Shopify Integration
+
+This is the codebase I use to integrate Shopify with Sendy. This allows me to upload the subscribers and customers to the mailing list. This process runs every hour.
+
+## Technology
+
+- Node 8
+- NPM
+
+## Created by
+
+[Stanley Yang](https://github.com/stanleycyang). Follow me for more great technology.
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const shopifyAPI = require('shopify-node-api');
 const request = require('request');
+const cron = require('cron');
 
 const Shopify = new shopifyAPI({
   shop: process.env.SHOP,
@@ -10,64 +11,71 @@ const Shopify = new shopifyAPI({
   access_token: process.env.ACCESS_TOKEN,
 });
 
-Shopify.get('/admin/customers.json', {}, function(err, data, headers){
+// add to mailing list
+function getMailingList() {
 
-  const promises = data.customers.map((customer, index) => {
-    const name = `${customer.first_name} ${customer.last_name}`;
-    const email = customer.email;
-    const boolean = true;
+  Shopify.get('/admin/customers.json', {}, function(err, data, headers){
 
-    console.log('name', name);
-    console.log('email', email);
+    const promises = data.customers.map((customer, index) => {
+      const name = `${customer.first_name} ${customer.last_name}`;
+      const email = customer.email;
+      const boolean = true;
 
-    if ((customer.orders_count > 0) && customer.first_name && customer.last_name) {
-      // if they are a customer
-      return new Promise((resolve, reject) => {
-        return request.post({
-          url: 'https://noreply.testimon.io/subscribe',
-          credentials: 'include',
-          form: {
-            email,
-            name,
-            list: process.env.BUYER_LIST,
-            boolean,
-          },
-        }, (err, response, body) => {
-          if (err) {
-            return reject(err);
-          }
+      console.log('name', name);
+      console.log('email', email);
 
-          resolve(body);
+      if ((customer.orders_count > 0) && customer.first_name && customer.last_name) {
+        // if they are a customer
+        return new Promise((resolve, reject) => {
+          return request.post({
+            url: 'https://noreply.testimon.io/subscribe',
+            credentials: 'include',
+            form: {
+              email,
+              name,
+              list: process.env.BUYER_LIST,
+              boolean,
+            },
+          }, (err, response, body) => {
+            if (err) {
+              return reject(err);
+            }
+
+            resolve(body);
+          });
         });
-      });
-    } else {
-      // if they are a subscriber
-      return new Promise((resolve, reject) => {
-        return request.post({
-          url: 'https://noreply.testimon.io/subscribe',
-          credentials: 'include',
-          form: {
-            email,
-            list: process.env.SUBSCRIBER_LIST,
-            boolean,
-          },
-        }, (err, response, body) => {
-          if (err) {
-            return reject(err);
-          }
+      } else {
+        // if they are a subscriber
+        return new Promise((resolve, reject) => {
+          return request.post({
+            url: 'https://noreply.testimon.io/subscribe',
+            credentials: 'include',
+            form: {
+              email,
+              list: process.env.SUBSCRIBER_LIST,
+              boolean,
+            },
+          }, (err, response, body) => {
+            if (err) {
+              return reject(err);
+            }
 
-          resolve(body);
+            resolve(body);
+          });
         });
-      });
-    }
+      }
 
+
+    });
+
+    return Promise.all(promises).then(result => {
+      console.log(result);
+    }).catch(err => {
+      console.error(err);
+    });
 
   });
 
-  return Promise.all(promises).then(result => {
-    console.log(result);
-  }).catch(err => {
-    console.error(err);
-  });
+}
 
-});
+const cronJob = new cron.CronJob('0 * * * *', getMailingList, null, true);
